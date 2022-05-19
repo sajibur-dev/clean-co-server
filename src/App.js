abdd32a52f11c528bc258a2b2d4fbdb3d5d99aa4
sajibur-dev <sajibur.dev@gmail.com>
2022-05-19 19:58:44 +0600

    navbar is done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Services from "./pages/Services";
 
 function App() {
   return (
-    <div>
-      <h1>clean co</h1>
-    </div>
+    <Navbar>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/services" element={<Services/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+        <Route path="/login" element={<Login/>}/>
+      </Routes>
+    </Navbar>
   );
 }
 
---
+++ src/pages/Services.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Services = () => {
+    return (
+        <div>
+            {/* this is services */}
+            <button className='dark:bg-primary'>btn</button>
+        </div>
+    );
+};
+
+export default Services;
